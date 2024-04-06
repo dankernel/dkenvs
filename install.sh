790f294e4b849903abbeb74a8d6b7045c116c313
dankernel <dkdkernel@gmail.com>
2024-04-06 13:23:53 +0900

    FEAT: Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,34 @@
+#!/bin/bash
 
-echo -e "\e[32mInstall bashrc"
+# Color code
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}Install bashrc${NC}"
 cp ~/.bashrc ~/.bashrc.old
 cp config/.bashrc ~/.bashrc
-echo -e "\e[32mInstall bashrc ok.."
-echo -e "\e[34mRun the following command in terminal:"
-echo -e "\e[37m $ . ~/.bashrc"
+echo -e "${YELLOW}Install bashrc ok..${NC}"
+echo -e "${RED}Run the following command in terminal:${NC}"
+echo -e "${RED} $ . ~/.bashrc${NC}"
 
-echo -e "\e[32mInstall Vundle"
+echo -e "${YELLOW}Install Vundle${NC}"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo -e "\e[32mInstall Vundle ok.."
+echo -e "${YELLOW}Install Vundle ok..${NC}"
 
-echo -e "\e[32mInstall vimrc"
+echo -e "${YELLOW}Install vimrc${NC}"
 cp ~/.vimrc ~/.vimrc.old
 cp config/.vimrc ~/.vimrc
-echo -e "\e[32mInstall vimrc ok.."
-echo -e "\e[34mRun the following command in terminal:"
-echo -e "\e[37m $ vim"
-echo -e "\e[37m $ :PluginInstall"
+echo -e "${YELLOW}Install vimrc ok..${NC}"
+echo -e "${RED}Run the following command in terminal:${NC}"
+echo -e "${RED} $ vim${NC}"
+echo -e "${RED} $ :PluginInstall${NC}"
 
-echo -e "\e[32mInstall shell script"
+echo -e "${YELLOW}Install shell script"
 cp -R bash ~/
-echo -e "\e[32mInstall shell script ok.."
-echo -e "\e[34mRun the following command in terminal:"
-echo -e "\e[37m $ vim bash/notifications/config_sample.ini"
-echo -e "\e[34mAnd cp bash/notifications/config_sample.ini bash/notifications/config.ini"
+echo -e "${YELLOW}Install shell script ok.."
